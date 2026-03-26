c21cb31b34151ee46adbae55e31d4842ae9caa24
ksenia <kote13241@gmail.com>
2026-03-26 21:00:59 +0400

    docker

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import app from "./app/app.js";
 import { config } from "./app/config.js";
 
-app.listen(config.port, () => {
-  console.log(`Server is running on http://localhost:${config.port}`);
+app.listen(config.port, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${config.port}`);
 });
